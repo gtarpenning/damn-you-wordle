3ecc1f2a0cfda1cfa47cd96ab4cd3e15c311b6f4
gtarpenning <gtarpenning@gmail.com>
2022-02-22 15:26:44 -0800

    better error handling

--- deploy/backend/handler.py
+++ deploy/backend/handler.py
@@ -24,6 +24,11 @@ def wordle_request(w_request):
     answers_left = w_request.answers_left
     allowed_left = w_request.allowed_left
 
+    if guess not in answers + allowed:
+        return {
+            "best_guess": "Word Not Found",
+        }
+
     if not answers_left or allowed_left:
         answers_left = candidate_lookup[guess][template]
         allowed_left = narrow_down(guess, template, allowed.copy(), template_lookup)
